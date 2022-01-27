df3e15aa3f0ce25e4ec2307e2690d676f2362600
Pierce <piercedavidraphael@gmail.com>
2022-01-27 09:42:37 -0600

    Changed play and pause back to original method

--- client/src/BeatButtons.js
+++ client/src/BeatButtons.js
@@ -5,26 +5,28 @@ function BeatButtons({ audioArray, setAudioArray }) {
 
   function play(e) {
     let audioPlay = new Audio(e.target.value);
-    // console.log(audioPlay)
-    audioPlay.volume = (e.target.children[2].value / 10)
-    // console.log((e.target.children[2].value / 10))
-    audioPlay.play();
-    // setAudioArray([...audioArray, audioPlay]);
+    audioPlay.volume = parseFloat(e.target.children[2].value / 10);
+
+    if (audioPlay.src === audioArray.includes(audioPlay.src)) {
+      audioPlay.play();
+    } else {
+      setAudioArray([...audioArray, audioPlay]);
+      audioPlay.play();
+    }
     // console.log(audioArray);
   }
 
-  // function pause(e) {
-  //   let pauseAudio = document.getElementById(e.target.value)
-  //   // console.log(pauseAudio)
-  //   pauseAudio.pause()
-
-  // }
+  function pause() {
+    for (let i = 0; i < audioArray.length; i++) {
+      audioArray[i].pause()
+      setAudioArray([])
+    }
+  }
 
   function keyPlay(e) {
     let i = parseInt(e.key);
 
     let keyAudioPlay = new Audio(audioArray[i].audio_url);
-    keyAudioPlay.volume = (e.target.children[2].value / 10)
     // console.log(audioArray[i].audio_url)
     // setAudioArray([...audioArray, keyAudioPlay]);
     // keyAudioPlay.volume = parseFloat(e.target.children[1].value / 10)
@@ -32,11 +34,11 @@ function BeatButtons({ audioArray, setAudioArray }) {
     keyAudioPlay.play();
   }
 
-  // console.log(audioArray)
+  console.log(audioArray)
 
   return (
     <div className="button-container" onKeyDown={(e) => keyPlay(e)}>
-      
+      <button id="pause" value={sound.name} onClick={() => pause()}>Pause All Tracks</button>
       {audioArray.map((sound) => {
         return (
           <div key={sound.id} className="beatbutton">
@@ -50,7 +52,7 @@ function BeatButtons({ audioArray, setAudioArray }) {
                 placeholder="Volume (1 ...)"
                 defaultValue={volume}
               />
-              {/* <button id="pause" value={sound.name} onClick={(e) => pause(e)}>Pause All Tracks</button> */}
+              
             </button>
             
           </div>
