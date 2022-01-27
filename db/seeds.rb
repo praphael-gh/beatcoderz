e85f80836fa0fda30b4a273eaca6c1787e199037
Pierce <piercedavidraphael@gmail.com>
2022-01-26 19:26:22 -0600

    Working on seeds 4 Final?

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,21 +28,21 @@ sound4.audio.attach(
 # # Synthwave sounds
 sound5 = Sound.create(name:"synth_kick2", user_id: 1, sound_pack_id: 2)
 sound5.audio.attach(
-    io: File.open('/home/sousman/Development/code/Mod5/project/beatcoderz/client/public/sound_files/kick2.wav'),
+    io: File.open(Rails.root.to_s + '/public/sound_files/kick2.wav'),
     filename: 'kick2.wav'
   )
 sound6 = Sound.create(name:"synth_snare2", user_id: 1, sound_pack_id: 2)
 sound6.audio.attach(
-    io: File.open('/home/sousman/Development/code/Mod5/project/beatcoderz/client/public/sound_files/snare2.wav'),
+    io: File.open(Rails.root.to_s + '/public/sound_files/snare2.wav'),
     filename: 'snare2.wav'
   )
 sound7 = Sound.create(name:"synth_hat2", user_id: 1, sound_pack_id: 2)
 sound7.audio.attach(
-    io: File.open('/home/sousman/Development/code/Mod5/project/beatcoderz/client/public/sound_files/hat2.wav'),
+    io: File.open(Rails.root.to_s + '/public/sound_files/hat2.wav'),
     filename: 'hat2.wav'
   )
 sound8 = Sound.create(name:"synth2", user_id: 1, sound_pack_id: 2)
 sound8.audio.attach(
-    io: File.open('/home/sousman/Development/code/Mod5/project/beatcoderz/client/public/sound_files/synth2.wav'),
+    io: File.open(Rails.root.to_s + '/public/sound_files/synth2.wav'),
     filename: 'synth2.wav'
   )
