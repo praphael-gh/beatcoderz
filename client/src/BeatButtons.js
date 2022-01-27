76cb39dd198966f3ce6a9616c9f23a4bbd4aad9f
Pierce <piercedavidraphael@gmail.com>
2022-01-27 08:44:21 -0600

    Volume not working

--- client/src/BeatButtons.js
+++ client/src/BeatButtons.js
@@ -4,42 +4,34 @@ function BeatButtons({ audioArray, setAudioArray }) {
   let volume = 4;
 
   function play(e) {
-    // let audioPlay = e.target.children[0]
-    // audioPlay.cloneNode().play()
     let audioPlay = new Audio(e.target.value);
-    // audioPlay.volume = parseFloat(e.target.children[1].value / 10);
-    audioPlay.play()
-    // console.log(e.target.children[1].value)
-    // if audioPlay.src = .src in audioArray
-    // just play audioPlay, otherwise add to audioArray
-  //   if (audioPlay.src === audioArray.includes(audioPlay.src)) {
-  //     audioPlay.play();
-  //   } else {
-  //     setAudioArray([...audioArray, audioPlay]);
-  //     audioPlay.play();
-  //   }
-  //   console.log(audioArray);
-  // }
+    // console.log(audioPlay)
+    audioPlay.volume = parseFloat(e.target.children[1].value / 10);
+    console.log(e.target.children[1].value / 10)
+    setAudioArray([...audioArray, audioPlay]);
+    audioPlay.play();
+    // console.log(audioArray);
+  }
 
   // function pause(e) {
   //   let pauseAudio = document.getElementById(e.target.value)
   //   // console.log(pauseAudio)
   //   pauseAudio.pause()
 
-  }
+  // }
 
   function keyPlay(e) {
     let i = parseInt(e.key);
 
     let keyAudioPlay = new Audio(audioArray[i].audio_url);
-    console.log(audioArray[i].audio_url)
+    // console.log(audioArray[i].audio_url)
     // setAudioArray([...audioArray, keyAudioPlay]);
     // keyAudioPlay.volume = parseFloat(e.target.children[1].value / 10)
     // keyAudioPlay.load()
     keyAudioPlay.play();
   }
 
-  console.log(audioArray)
+  // console.log(audioArray)
 
   return (
     <div className="button-container" onKeyDown={(e) => keyPlay(e)}>
