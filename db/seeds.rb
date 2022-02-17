a4d7ea9c7946f77a1cb68181528782dc742dd00e
Pierce <piercedavidraphael@gmail.com>
2022-02-16 18:14:54 -0600

    Troubleshooting errors with password_digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+User.destroy_all
+
 user1 = User.create(username:"Joe", password:"ya")
 user2 = User.create(username:"Moe", password:"yo")
+
+Song.destroy_all
 
 song1 = SoundPack.create(name:"lofi", genre:"lofi", user_id: User.first.id)
 song2 = SoundPack.create(name:"Synthwave", genre: "Synthwave", user_id: User.first.id)
