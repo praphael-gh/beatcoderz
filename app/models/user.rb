d5647f62b283e25dccfe634d1791c9826166db22
Pierce <piercedavidraphael@gmail.com>
2022-02-21 10:39:29 -0600

    ahhhhh added include ActiveModel::SecurePassword in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
+    include ActiveModel::SecurePassword 
     has_many :sounds
     has_many :sound_packs
 
     validates :username, uniqueness: true
     # validates :password_digest, presence: true
     
-    # attr_accessor :password_digest
+    attr_accessor :password_digest
 end
